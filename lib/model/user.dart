e07ab6901061916ca86972e55f01d1844f9ee702
ardhikaxx <ardhikayanuar58@gmail.com>
2024-04-27 08:51:44 +0700

    update-login dan register menggunakan api

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,21 +1,37 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
 class UserData {
-  final int id;
-  final String email;
-  final String nikIbu;
+  final int nikIbu;
   final String namaIbu;
-  final String tempatLahir;
-  final String tanggalLahir;
+  final String tempatLahirIbu;
+  final String tanggalLahirIbu;
+  final String golDarahIbu;
+  final String nikAyah;
+  final String namaAyah;
   final String alamat;
-  final String telepon;
+  final int telepon;
+  final String emailOrangTua;
+  final String createdAt;
+  final String updatedAt;
 
   UserData({
-    required this.id,
-    required this.email,
     required this.nikIbu,
     required this.namaIbu,
-    required this.tempatLahir,
-    required this.tanggalLahir,
+    required this.tempatLahirIbu,
+    required this.tanggalLahirIbu,
+    required this.golDarahIbu,
+    required this.nikAyah,
+    required this.namaAyah,
     required this.alamat,
     required this.telepon,
+    required this.emailOrangTua,
+    required this.createdAt,
+    required this.updatedAt,
   });
+
+  factory UserData.fromJson(Map<String, dynamic> json) => _$UserDataFromJson(json);
+  Map<String, dynamic> toJson() => _$UserDataToJson(this);
 }
